c2871432eeb5f7cad33afd6a6306cc457992aeb2
Your Name <lianshijie@3023668879.com>
2019-04-10 21:13:40 +0800

    vim

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    holle  cuifang
+                    holle fangfang
                 </div>
 
                 <div class="links">
